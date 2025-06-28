1581187a5b88bcd6e5e36a93cbbf2dae3adc4d6c
NIMISH SHIRODKAR <nimish.shirodkar@gmail.com>
2025-06-28 11:15:34 +0000

    Merge Failed: An unexpected response was received from the server

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverActions: {
-    bodySizeLimit: '25mb',
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '25mb',
+    },
   },
   typescript: {
     ignoreBuildErrors: true,
